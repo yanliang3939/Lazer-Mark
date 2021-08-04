51ce1f5e8e3e8bcc45e1d09be6017b0560998e5b
liuyanliang <1528210361@qq.com>
2021-08-04 09:31:24 +0800

    添加项目文件。

---
+++ TCP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            IPAddress iPAddress = new IPAddress(new byte[] { 192, 168, 37, 1 });
+            EndPoint point = new IPEndPoint(iPAddress, 5566);
+            tcpServer.Bind(point);
+
+            tcpServer.Listen(100);
+            Console.WriteLine("ready");
+
+            Socket cilientSocket = tcpServer.Accept();
+            Console.WriteLine("link");
+
+            string message = "hello";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            cilientSocket.Send(data);
+            Console.WriteLine("send message:",message);
+
+            byte[] data2 = new byte[1024];
+            int length = cilientSocket.Receive(data2);
+            string message2 = Encoding.UTF8.GetString(data2, 0, length);
+            Console.WriteLine("reciieve a message:" + message2);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
